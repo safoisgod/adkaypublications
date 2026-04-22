ca9ed1936d10a0e74aff170a00dd4c20c70e76ed
Nana Safo <iamnanasafo@gmail.com>
2026-04-22 16:36:46 +0000

    cleaned settings.py emails

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -265,8 +265,8 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@publishinghouse.com')
-ADMIN_EMAIL = config('ADMIN_EMAIL', default='admin@publishinghouse.com')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@adkaypublications.com')
+ADMIN_EMAIL = config('ADMIN_EMAIL', default='admin@adkaypublications.com')
 
 # ─────────────────────────────────────────
 # SITE META
